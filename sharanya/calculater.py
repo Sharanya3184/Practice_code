678e1baec9553d970cc0fce86b59b804a5b1a356
sharanya3184 <sharanya3184@gmail.com>
2025-05-05 14:53:54 +0530

    Oops coding updated

---
+++ sharanya/calculater.py
@@ -0,0 +1,28 @@
+class Calculator:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def add(self):
+        return self.num1 + self.num2
+
+    def subtract(self):
+        return self.num1 - self.num2
+
+    def multiply(self):
+        return self.num1 * self.num2
+
+    def divide(self):
+        
+        if self.num2 != 0:
+            return self.num1 / self.num2
+        else:
+            return "Cannot divide by zero"
+
+
+calc = Calculator(self, num1=10, num2=5)
+
+print("Addition       :", calc.add())
+print("Subtraction    :", calc.subtract())
+print("Multiplication :", calc.multiply())
+print("Division       :", calc.divide())
